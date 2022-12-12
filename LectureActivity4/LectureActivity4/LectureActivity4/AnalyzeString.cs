3697c730b1511814ccb72f2ca0f7c6118ea96715
arcanehack <7907763+arcanehack@users.noreply.github.com>
2022-12-12 00:23:23 -0700

    Initial commit

---
+++ LectureActivity4/LectureActivity4/LectureActivity4/AnalyzeString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LectureActivity4
+{
+    class AnalyzeString
+    {
+        static void Main(string[] args)
+        {
+            
+            string str = Console.ReadLine();
+            StringAnalysis analysis = new StringAnalysis();
+            
+            Thread digitThread = new Thread(() => analysis.upperCountThread(str));
+            Thread upperThread = new Thread(() => analysis.DigitCountThread(str));
+            Thread palThread = new Thread(() => analysis.isPalindromeThread(str));
+
+            digitThread.Start();
+            upperThread.Start();
+            palThread.Start();
+            digitThread.Join();
+            upperThread.Join();
+            palThread.Join();
+
+            Console.WriteLine("Digit count: " + analysis.getDigitCount() + "\n" +
+                            "Upper count: " + analysis.getUpperCount() + "\n" +
+                            "Is it a palindrome: " + analysis.getPalindrome());
+        }
+    }
+
+
+}
---
+++ LectureActivity4/LectureActivity4/LectureActivity4/StringAnalysis.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+public class StringAnalysis
+{
+    protected bool isPalindrome = false;
+    private int upperCount = 0;
+    private int digitCount = 0;
+    public void isPalindromeThread(String str)
+    {
+
+        str = str.ToLower().Replace("[\\s|,|.|?|;|']", "");
+        if (str.Equals(new string(str.Reverse().ToArray()))) //source: https://stackoverflow.com/a/9791036
+            isPalindrome = true;
+
+    }
+
+    public bool getPalindrome()
+    {
+        return isPalindrome;
+    }
+
+    public void upperCountThread(String str)
+    {
+        
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsUpper(str[i]))
+            {
+                upperCount++;
+            }
+        }
+    }
+    public int getUpperCount()
+    {
+        return upperCount;
+    }
+
+
+    public void DigitCountThread(string str)
+    {
+        
+        for (int i = 0; i < str.Length; i++)
+        {
+
+            if (char.IsDigit(str[i]))
+            {
+                digitCount++;
+            }
+        }
+    }
+    public int getDigitCount()
+    {
+        return digitCount;
+    }
+}
